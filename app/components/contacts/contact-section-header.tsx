c018fdbce47319b641eba0e42c13067ca4d1594e
Alexandra Marin <alexandra.marin@workjam.com>
2019-01-23 14:51:34 +0200

    WIP: section head

--- app/components/contacts/contact-section-header.tsx
+++ app/components/contacts/contact-section-header.tsx
@@ -1,26 +1,29 @@
-import PropTypes from 'prop-types';
 import React from 'react';
 import { observer } from 'mobx-react/native';
-import { View } from 'react-native';
+import { View, ViewStyle } from 'react-native';
 import SafeComponent from '../shared/safe-component';
 import { vars } from '../../styles/styles';
 import Text from '../controls/custom-text';
 
+const style: ViewStyle = {
+    height: vars.sectionHeaderHeight,
+    justifyContent: 'center',
+    backgroundColor: vars.darkBlueBackground05
+};
+
+const textStyle = {
+    marginLeft: vars.spacing.large.mini,
+    color: vars.txtMedium
+};
+
+interface ContactSectionHeaderProps {
+    title: string;
+}
 @observer
-export default class ContactSectionHeader extends SafeComponent {
+export default class ContactSectionHeader extends SafeComponent<ContactSectionHeaderProps> {
     renderThrow() {
         const { title } = this.props;
         if (!title) return null;
-        const style = {
-            height: vars.sectionHeaderHeight,
-            justifyContent: 'center',
-            backgroundColor: vars.darkBlueBackground05
-        };
-
-        const textStyle = {
-            marginLeft: vars.spacing.large.mini,
-            color: vars.txtMedium
-        };
         return (
             <View style={style}>
                 <Text bold style={textStyle}>
@@ -30,7 +33,3 @@ export default class ContactSectionHeader extends SafeComponent {
         );
     }
 }
-
-ContactSectionHeader.propTypes = {
-    title: PropTypes.any
-};
