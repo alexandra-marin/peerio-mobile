8060a112447b7dc6fbd707de936bd9c9fc844cb2
Alexandra Marin <alexandra.marin@workjam.com>
2019-01-23 13:40:33 +0200

    WIP: contacts placeholder

--- app/components/contacts/contacts-placeholder.tsx
+++ app/components/contacts/contacts-placeholder.tsx
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 import { observer } from 'mobx-react/native';
-import { View } from 'react-native';
+import { View, ViewStyle } from 'react-native';
 import Text from '../controls/custom-text';
 import BlueButtonText from '../buttons/blue-text-button';
 import { t } from '../utils/translator';
 import contactState from './contact-state';
+
+const outer = {
+    flex: 1,
+    flexGrow: 1
+};
+const inner: ViewStyle = {
+    flex: 1,
+    flexGrow: 2,
+    justifyContent: 'center',
+    alignItems: 'center'
+};
 
 @observer
 export default class ContactsPlaceholder extends Component {
@@ -14,18 +25,8 @@ export default class ContactsPlaceholder extends Component {
     }
 
     render() {
-        const s = {
-            flex: 1,
-            flexGrow: 1
-        };
-        const inner = {
-            flex: 1,
-            flexGrow: 2,
-            justifyContent: 'center',
-            alignItems: 'center'
-        };
         return (
-            <View style={s}>
+            <View style={outer}>
                 <View style={inner}>
                     <Text>{t('title_importInviteText')}</Text>
                     <BlueButtonText text="button_importOrInvite" onPress={this.importOrInvite} />
