1bb01edbe1efcce0275a6e3b86a754dc70e8fe55
Samvel Avanesov <seavan@gmail.com>
2019-02-27 15:37:51 -0500

    WIP: release npm script

---
+++ scripts/release-module-ios.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+rm -rf dist
+mkdir dist
+mkdir dist/debug
+cp -r node_modules/\@workjam/react-native-chat-framework/ios/build/debug/* dist/debug/
+
+source env.sh
+
+./node_modules/.bin/react-native bundle --entry-file index.ios.js --bundle-output dist/debug/main.jsbundle --assets-dest "dist/debug" --platform ios --reset-cache
+
+mkdir dist/release
+
+./node_modules/.bin/react-native bundle --dev false --entry-file index.ios.js --bundle-output dist/release/main.jsbundle --assets-dest "dist/release" --platform ios --reset-cache
+
+mkdir dist/fonts
+cp -r app/assets/fonts/* dist/fonts/
+cp -r node_modules/react-native-vector-icons/Fonts/* dist/fonts/
+chmod 644 dist/fonts/*
+
+cp -r app/sounds dist/
